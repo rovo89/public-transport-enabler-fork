cdae04c8043f11e3726acb5b747f06e105a5596f
Andreas Schildbach <andreas@schildbach.de>
2012-07-12 12:48:08 +0200

    try at repairing nasa/insa which is using h2g-direct=11 now (gzip compress binary format)

--- enabler/src/de/schildbach/pte/NasaProvider.java
+++ enabler/src/de/schildbach/pte/NasaProvider.java
@@ -17,13 +17,24 @@
 
 package de.schildbach.pte;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import java.util.zip.GZIPInputStream;
 
 import de.schildbach.pte.dto.Location;
 import de.schildbach.pte.dto.LocationType;
 import de.schildbach.pte.dto.NearbyStationsResult;
+import de.schildbach.pte.dto.QueryConnectionsResult;
 import de.schildbach.pte.dto.QueryDeparturesResult;
+import de.schildbach.pte.util.ParserUtils;
 
 /**
  * @author Andreas Schildbach
@@ -162,6 +173,135 @@ public class NasaProvider extends AbstractHafasProvider
 	public List<Location> autocompleteStations(final CharSequence constraint) throws IOException
 	{
 		return xmlMLcReq(constraint);
+	}
+	
+	private String connectionsQueryUri(final Location from, final Location via, final Location to, final Date date, final boolean dep,
+			final String products)
+	{
+		final Calendar c = new GregorianCalendar(timeZone());
+		c.setTime(date);
+
+		final StringBuilder uri = new StringBuilder();
+		
+		// clientSystem=Android7&REQ0JourneyProduct_prod_list_1=11111111111&timeSel=depart&hcount=0&date=11.07.2012&ignoreMinuteRound=yes&androidversion=1.0.2&SID=A%3d1%40O%3dLeipzig%20Hbf%40X%3d12383333%40Y%3d51346546%40U%3d80%40L%3d008010205%40B%3d1%40V%3d12.9,%40p%3d1341849783%40&h2g-direct=11&time=18%3a04&REQ0HafasNumCons0=3&start=1&REQ0HafasNumCons1=0&clientDevice=Milestone&REQ0HafasNumCons2=3&htype=Milestone&ZID=A%3d1%40O%3dDresden%20Hbf%40X%3d13732038%40Y%3d51040562%40U%3d80%40L%3d008010085%40B%3d1%40V%3d12.9,%40p%3d1341849783%40&clientType=ANDROID
+
+		uri.append(API_BASE).append("query.exe/dn");
+
+		uri.append("?start=Suchen");
+
+		uri.append("&REQ0JourneyStopsS0ID=").append(ParserUtils.urlEncode(locationId(from), ISO_8859_1));
+		uri.append("&REQ0JourneyStopsZ0ID=").append(ParserUtils.urlEncode(locationId(to), ISO_8859_1));
+
+		if (via != null)
+		{
+			// workaround, for there does not seem to be a REQ0JourneyStops1.0ID parameter
+
+			uri.append("&REQ0JourneyStops1.0A=").append(locationType(via));
+
+			if (via.type == LocationType.STATION && via.hasId() && isValidStationId(via.id))
+			{
+				uri.append("&REQ0JourneyStops1.0L=").append(via.id);
+			}
+			else if (via.hasLocation())
+			{
+				uri.append("&REQ0JourneyStops1.0X=").append(via.lon);
+				uri.append("&REQ0JourneyStops1.0Y=").append(via.lat);
+				if (via.name == null)
+					uri.append("&REQ0JourneyStops1.0O=").append(
+							ParserUtils.urlEncode(String.format(Locale.ENGLISH, "%.6f, %.6f", via.lat / 1E6, via.lon / 1E6), ISO_8859_1));
+			}
+			else if (via.name != null)
+			{
+				uri.append("&REQ0JourneyStops1.0G=").append(ParserUtils.urlEncode(via.name, ISO_8859_1));
+				if (via.type != LocationType.ANY)
+					uri.append('!');
+			}
+		}
+
+		uri.append("&REQ0HafasSearchForw=").append(dep ? "1" : "0");
+		uri.append("&REQ0JourneyDate=").append(
+				String.format("%02d.%02d.%02d", c.get(Calendar.DAY_OF_MONTH), c.get(Calendar.MONTH) + 1, c.get(Calendar.YEAR) - 2000));
+		uri.append("&REQ0JourneyTime=").append(String.format("%02d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
+
+		for (final char p : products.toCharArray())
+		{
+			if (p == 'I')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_5=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_5=1");
+			}
+			if (p == 'R')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_6=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_6=1");
+			}
+			if (p == 'S')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_0=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_0=1");
+			}
+			if (p == 'U')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_1=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_1=1");
+			}
+			if (p == 'T')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_2=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_2=1");
+			}
+			if (p == 'B')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_3=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_3=1");
+			}
+			if (p == 'P')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_7=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_7=1");
+			}
+			if (p == 'F')
+			{
+				uri.append("&REQ0JourneyProduct_prod_section_0_4=1");
+				if (via != null)
+					uri.append("&REQ0JourneyProduct_prod_section_1_4=1");
+			}
+		}
+		
+		uri.append("&h2g-direct=11");
+
+		return uri.toString();
+	}
+
+	
+	@Override
+	public QueryConnectionsResult queryConnections(Location from, Location via, Location to, Date date, boolean dep, int numConnections,
+			String products, WalkSpeed walkSpeed, Accessibility accessibility, Set<Option> options) throws IOException
+	{
+		final String uri = connectionsQueryUri(from, via, to, date, dep, products);
+		
+		System.out.println("===== TRYING: " + uri);
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(ParserUtils.scrapeInputStream(uri), ISO_8859_1));
+		
+		while(true)
+		{
+			String line = reader.readLine();
+			if(line == null)
+				break;
+			
+			System.out.println(line);
+		}
+		
+		// TODO Auto-generated method stub
+		return super.queryConnections(from, via, to, date, dep, numConnections, products, walkSpeed, accessibility, options);
 	}
 
 	@Override
